5daeff62457ab3db9b52771cd33ee29970ded62a
Romi Landau <romi7102@gmail.com>
2024-02-11 16:56:54 +0200

    improved validation from mongo

--- main.py
+++ main.py
@@ -1,46 +1,41 @@
 import json
+import os
 from datetime import datetime
 from kafka import KafkaConsumer
 from pymongo import MongoClient 
 
-#todo: make the timestamp find only when starting up
-#todo: remove mongo insert func
-#todo: get cs from enviroment vars
 
-MONGO_CS = 'mongodb://admin:admin@localhost:27017' #! environment variable
+MONGO_CS = os.environ.get("MONGO_CS") #'mongodb://admin:admin@localhost:27017' 
 MONGO_CLIENT = MongoClient(MONGO_CS)
 DB = MONGO_CLIENT.testdb
 
 def main():
-    #! enviroment variables change me !
-    bootstrap_servers = 'localhost:9092'
-    kafka_topic = 'pyTest'
+    
+    bootstrap_servers = os.environ.get("BOOTSTRAP_SERVER") #'localhost:9092'
+    kafka_topic = os.environ.get("TOPIC") #'pyTest'
+
+    events = DB.events
+    most_recent_doc = events.find_one({}, sort=[("timestamp", -1)])
+    print(most_recent_doc)
 
     try:
         consumer = KafkaConsumer(kafka_topic, bootstrap_servers=bootstrap_servers,
                              auto_offset_reset='earliest') 
-        consumer.subscribe(['pyTest'])
+        consumer.subscribe([kafka_topic])
+        print(f"subscribed to {kafka_topic}")
 
         for message in consumer:
             json_str = message.value.decode('utf-8')
-            insert_mongo(json_str)
+            new_event = json.loads(json_str)
+            new_event["timestamp"] = datetime.strptime(new_event["timestamp"] ,"%Y-%m-%d %H:%M:%S")
+
+            if most_recent_doc is None or new_event["timestamp"] > most_recent_doc["timestamp"]:
+                events.insert_one(new_event)
+                most_recent_doc = new_event
+                print(new_event)
 
     except KeyboardInterrupt:
         consumer.close()
-
-def insert_mongo(json_str : str):
-    events = DB.events
-    most_recent_doc = events.find_one({}, sort=[("timestamp", -1)])
-
-    if most_recent_doc is not None: 
-        most_recent_date = most_recent_doc["timestamp"]
-
-    new_event = json.loads(json_str)
-    new_event["timestamp"] = datetime.strptime(new_event["timestamp"] ,"%Y-%m-%d %H:%M:%S")
-
-    if most_recent_doc is None or new_event["timestamp"] > most_recent_date:
-        events.insert_one(new_event)
-        print(new_event)
 
 if __name__ == '__main__':
     main()
