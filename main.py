74c457b04fc0c4ebe2d5460bf5d5991fa10a4547
Romi Landau <romi7102@gmail.com>
2024-02-15 16:09:37 +0200

    using consumer offset

--- main.py
+++ main.py
@@ -2,26 +2,35 @@ import json
 import os
 from datetime import datetime
 from kafka import KafkaConsumer
-from pymongo import MongoClient 
-
+from pymongo import MongoClient
 
 MONGO_CS = os.environ.get("MONGO_CS")
+MONGO_DB = os.environ.get("MONGO_DB")
+MONGO_COLLECTION = os.environ.get("MONGO_COLLECTION")
 MONGO_CLIENT = MongoClient(MONGO_CS)
-DB = MONGO_CLIENT.testdb
+DB = MONGO_CLIENT[MONGO_DB]
+EVENTS = DB[MONGO_COLLECTION]
+
+BOOTSTRAP_SERVERS = os.environ.get("BOOTSTRAP_SERVER")
+KAFKA_TOPIC = os.environ.get("TOPIC")
+KAFKA_GROUP_ID = os.environ.get("KAFKA_GROUP_ID")
 
 def main():
-    bootstrap_servers = os.environ.get("BOOTSTRAP_SERVER")
-    kafka_topic = os.environ.get("TOPIC")
 
-    events = DB.events
-    most_recent_doc = events.find_one({}, sort=[("timestamp", -1)])
-    print(most_recent_doc)
+    auto_offset_reset = 'earliest'
+    if EVENTS.find_one({}):
+        auto_offset_reset = 'latest'
 
     try:
-        consumer = KafkaConsumer(kafka_topic, bootstrap_servers=bootstrap_servers,
-                             auto_offset_reset='earliest') 
-        consumer.subscribe([kafka_topic])
-        print(f"subscribed to {kafka_topic}")
+        consumer = KafkaConsumer(
+            KAFKA_TOPIC, 
+            group_id=KAFKA_GROUP_ID,
+            bootstrap_servers=BOOTSTRAP_SERVERS,
+            auto_offset_reset=auto_offset_reset,
+            enable_auto_commit= False) 
+        
+        consumer.subscribe([KAFKA_TOPIC])
+        print(f"subscribed to {KAFKA_TOPIC}")
 
         for message in consumer:
             try:
@@ -29,12 +38,12 @@ def main():
                 new_event = json.loads(json_str)
             except Exception:
                 continue
+
             new_event["timestamp"] = datetime.strptime(new_event["timestamp"] ,"%Y-%m-%d %H:%M:%S")
 
-            if most_recent_doc is None or new_event["timestamp"] > most_recent_doc["timestamp"]:
-                events.insert_one(new_event)
-                most_recent_doc = new_event
-                print(new_event)
+            EVENTS.insert_one(new_event)
+            print(new_event)
+            consumer.commit()
 
     except Exception:
         consumer.close()
